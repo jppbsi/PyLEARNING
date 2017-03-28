121cd4af56331c68668c30324689ebdbf9fafa3d
Joao Papa <papa@Joaos-MacBook-Air-2.local>
2017-03-27 21:25:43 -0300

    Working on linear regression.

--- Datasets/generate_2D_regression.py
+++ Datasets/generate_2D_regression.py
@@ -8,18 +8,24 @@ from sklearn.datasets import make_regression
 def main():
     
     parser = argparse.ArgumentParser(description='2D Synthetic dataset generation for regression problems')
-    parser.add_argument('-n','--n_samples', help='number of samples', required=True)
+    parser.add_argument('-s','--samples', help='number of samples', required=True)
+    parser.add_argument('-n','--noise', help='noise? (yes|no)', required=True)
     parser.add_argument('-o','--output', help='output file name', required=True)
+    
     args = vars(parser.parse_args())
     
     n_samples = int(sys.argv[2])
     n_features = 1
     
-    plt.title("Synthetic Dataset", fontsize='small')
-    X, Y = make_regression(n_samples = n_samples, n_features = n_features, n_informative = 1, noise = 10, random_state = 0)
-    plt.scatter(X, Y, color='blue', marker='.')
-    plt.show()
-    np.savetxt(sys.argv[4], np.c_[Y, np.array(X)], fmt = '%.4f', header = str(n_samples)+' '+str(n_features), comments = '')
+    plt.title("Synthetic Dataset", fontsize='small')    
+    if(sys.argv[4].upper() == "YES"):
+        X, Y = make_regression(n_samples = n_samples, n_features = n_features, n_informative = 1, noise = 10, random_state = 0)
+    else:
+        X, Y = make_regression(n_samples = n_samples, n_features = n_features, n_informative = 1, noise = 0, random_state = 0)
     
+    plt.scatter(X, Y, color='blue', marker='o')
+    plt.show()
+    np.savetxt(sys.argv[6], np.c_[Y, np.array(X)], fmt = '%.4f', header = str(n_samples)+' '+str(n_features), comments = '')
+
 if __name__ == '__main__':
   main()
--- Regression/linear_regression.py
+++ Regression/linear_regression.py
@@ -1,18 +1,30 @@
 import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
 import argparse
 import csv
 import sys
 
-class Linear_Regression:
-    
-    def __init__(self, learning_rate = 0.1):
-        self.learning_rate = learning_rate
+def main():
 
-if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser(description='Regularized Linear Regression')
-    parser.add_argument('-tr','--training_data', help='training dataset', required=True)
-    parser.add_argument('-ts','--testing_data', help='testing dataset', required=True)
-    parser.add_argument('-lr','--learning_rate', help='learning rate', required=False)
+    parser = argparse.ArgumentParser(description='Linear Regression')
+    parser.add_argument('-d','--data', help='dataset', required=True)
     args = vars(parser.parse_args())
     
+    data = np.matrix(np.loadtxt(open(sys.argv[2], 'r'), delimiter=" ", skiprows=1))
+    
+    X = np.array(data[:,1])
+    Y = np.array(data[:,0])
+    
+    model = linear_model.LinearRegression()
+    model.fit(X, Y)
+    Y_prime = model.predict(X)
+    
+    plt.scatter(X, Y, color='blue', marker='o')
+    plt.plot(X, Y_prime, color='black', linewidth=2)
+    plt.show()
+    
+    print("Mean squared error: %.2f" % np.mean((Y_prime - Y) ** 2))
+    
+if __name__ == '__main__':
+  main()
